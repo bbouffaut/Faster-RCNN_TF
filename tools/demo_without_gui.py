172164f257ad171cd7a42f5d2972ea0883952fd4
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2017-06-10 00:05:14 +0200

    implement demo script that can run without GUI

---
+++ tools/demo_without_gui.py
@@ -0,0 +1,95 @@
+import _init_paths
+import tensorflow as tf
+from fast_rcnn.config import cfg
+from fast_rcnn.test import im_detect
+from fast_rcnn.nms_wrapper import nms
+from utils.timer import Timer
+import numpy as np
+import os, sys, cv2
+import argparse
+from networks.factory import get_network
+
+
+CLASSES = ('__background__',
+           'aeroplane', 'bicycle', 'bird', 'boat',
+           'bottle', 'bus', 'car', 'cat', 'chair',
+           'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant',
+           'sheep', 'sofa', 'train', 'tvmonitor')
+
+
+#CLASSES = ('__background__','person','bike','motorbike','car','bus')
+
+def demo(sess, net, image_name):
+    """Detect object classes in an image using pre-computed object proposals."""
+
+    # Load the demo image
+    im_file = os.path.join(cfg.DATA_DIR, 'demo', image_name)
+    #im_file = os.path.join('/home/corgi/Lab/label/pos_frame/ACCV/training/000001/',image_name)
+    im = cv2.imread(im_file)
+
+    # Detect all object classes and regress object bounds
+    timer = Timer()
+    timer.tic()
+    scores, boxes = im_detect(sess, net, im)
+    print(scores)
+    print(boxes)
+    timer.toc()
+    print ('Detection took {:.3f}s for '
+           '{:d} object proposals').format(timer.total_time, boxes.shape[0])
+
+    CONF_THRESH = 0.8
+    NMS_THRESH = 0.3
+    for cls_ind, cls in enumerate(CLASSES[1:]):
+	print(cls_ind,cls)
+        cls_ind += 1 # because we skipped background
+        cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+        cls_scores = scores[:, cls_ind]
+        dets = np.hstack((cls_boxes,
+                          cls_scores[:, np.newaxis])).astype(np.float32)
+        keep = nms(dets, NMS_THRESH)
+        dets = dets[keep, :]
+	print(dets)
+
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Faster R-CNN demo')
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
+                        default=0, type=int)
+    parser.add_argument('--cpu', dest='cpu_mode',
+                        help='Use CPU mode (overrides --gpu)',
+                        action='store_true')
+    parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16]',
+                        default='VGGnet_test')
+    parser.add_argument('--model', dest='model', help='Model path',
+                        default=' ')
+    parser.add_argument('im_name',required=True)
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+
+    args = parse_args()
+
+    if args.model == ' ':
+        raise IOError(('Error: Model not found.\n'))
+        
+    # init session
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+    # load network
+    net = get_network(args.demo_net)
+    # load model
+    saver = tf.train.Saver()
+    saver.restore(sess, args.model)
+    #sess.run(tf.initialize_all_variables())
+
+    print '\n\nLoaded network {:s}'.format(args.model)
+    
+
+    print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    print 'Demo for data/demo/{}'.format(im_name)
+    demo(sess, net, args.im_name)
