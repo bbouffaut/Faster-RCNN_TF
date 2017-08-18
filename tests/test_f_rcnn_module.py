f1d0c8b0fc63fcdaf6d02cade1fb4322e9149cbe
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2017-08-18 15:33:19 +0200

    slight updates for testing purpose

--- tests/test_f_rcnn_module.py
+++ tests/test_f_rcnn_module.py
@@ -1,7 +1,78 @@
 import _init_paths
 import faster_rcnn_tf as f_rcnn
+import threading
+import Queue
+import os
+from utils.timer import Timer
 
+exit_flag = 0
+
+class ImageProcessingThread(threading.Thread):
+	
+	def __init__(self,thread_id,input_queue):
+	    threading.Thread.__init__(self)
+	    self.thread_id = thread_id
+	    self.q = input_queue
+	
+	def run(self):
+	    process_input_queue(self.thread_id,self.q)
+
+
+def process_input_queue(thread_id, q):
+	while not exit_flag:
+	    input_queue_lock.acquire()
+	    if not q.empty():
+	        image_name = q.get()
+		input_queue_lock.release()
+		
+		proc_timer = Timer()
+		proc_timer.tic()
+		vis = f_rcnn.process_image(image_name)
+		proc_timer.toc()
+		print('{} processed image {} in {:.3f} for {:d} object detected'.format(thread_id, image_name, proc_timer.total_time, len(vis)))
+	    else:
+	        input_queue_lock.release()
+	    
+	    	
+input_queue_lock = threading.Lock()
+input_queue = Queue.Queue()
+threads = []
+
+nb_threads = 1
+images_list = ['000456.jpg','000542.jpg','001150.jpg','001763.jpg','004545.jpg','pedestrian_cars.jpg','000456.jpg','000542.jpg','001150.jpg','001763.jpg','004545.jpg','pedestrian_cars.jpg']
+
+timer = Timer()
+
+# initialize TF network
 f_rcnn.init_tf_network('/srv/workspace/Faster-RCNN_TF/data/pretrain_model/VGGnet_fast_rcnn_iter_70000.ckpt')
-vis = f_rcnn.process_image('./data/demo/004545.jpg')
 
-print([(v.class_name,v.score) for v in vis] )
+# Create new threads
+for t_name in [('Thread-' + str(i)) for i in range(nb_threads)]:
+   thread = ImageProcessingThread(t_name, input_queue)
+   thread.start()
+   threads.append(thread)
+
+# Fill the input queue with list of images => START Timer
+timer.tic()
+input_queue_lock.acquire()
+
+for image in images_list:
+    input_queue.put(os.path.join('/srv/workspace/Faster-RCNN_TF/data/demo/',image))
+
+input_queue_lock.release()
+
+# Wait for queue to empty
+while not input_queue.empty():
+    pass
+
+# Notify threads it's time to exit when queue is empty
+exit_flag = 1
+
+# Wait for all threads to complete
+for t in threads:
+   t.join()
+
+#Stop Timer
+timer.toc()
+
+print("Processing of {} images took {:.3f} sec".format(len(images_list),timer.total_time))
