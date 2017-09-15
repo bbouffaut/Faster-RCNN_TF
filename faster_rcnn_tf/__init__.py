357ffe8a8e5819093e71935da418c0643c80aff6
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2017-09-16 00:03:36 +0200

    update __init__.py to expose the right files

--- faster_rcnn_tf/__init__.py
+++ faster_rcnn_tf/__init__.py
@@ -4,3 +4,5 @@
 # Licensed under The MIT License [see LICENSE for details]
 # Written by Ross Girshick
 # --------------------------------------------------------
+from .handlers.handlers import EventsHandler
+from .faster_rcnn_tf import *
